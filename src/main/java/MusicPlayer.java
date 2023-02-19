3d511c4831fe42b1b7c94c8b97cd1cef30c619ca
Leonert <leonid.knyshev@gmail.com>
2023-02-19 23:38:03 +0200

    lists passing

--- src/main/java/MusicPlayer.java
+++ src/main/java/MusicPlayer.java
@@ -1,15 +1,38 @@
-public class MusicPlayer {
-    Music music;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+    private List<Music> musicList;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public MusicPlayer(List<Music> music) {
+        this.musicList = music;
+    }
+
+    public MusicPlayer(){}
+
     void playMusic() {
+        for (Music music: musicList)
         System.out.println(music.playMysic() + " is playing");
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 }
--- src/main/java/SpringTest.java
+++ src/main/java/SpringTest.java
@@ -8,6 +8,11 @@ public class SpringTest {
 //        TestBean testBean = context.getBean("TestBean", TestBean.class);
 //        System.out.println(testBean.getName());
 
-        context.getBean("musicPlayer", MusicPlayer.class).playMusic();
+        MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
+        mp.playMusic();
+//        System.out.println(mp.getName());
+//        System.out.println(mp.getVolume());
+
+        context.close();
     }
 }
