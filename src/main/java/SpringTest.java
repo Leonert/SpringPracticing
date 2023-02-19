ff07760015066abf5ffa988e3de488027f95847d
Leonert <leonid.knyshev@gmail.com>
2023-02-19 12:44:41 +0200

    start

---
+++ src/main/java/SpringTest.java
@@ -0,0 +1,10 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appcontext.xml");
+
+        TestBean testBean = context.getBean("TestBean", TestBean.class);
+        System.out.println(testBean.getName());
+    }
+}
