68ef3ca3eef112f4ac899c8d6dbb20f907f83911
Leonert <leonid.knyshev@gmail.com>
2023-02-20 12:43:41 +0200

    factory method

--- src/main/java/ClassicalMusic.java
+++ src/main/java/ClassicalMusic.java
@@ -1,6 +1,19 @@
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getInstance() {
+        System.out.println("factory method is working");
+        return new ClassicalMusic();
+    }
     @Override
     public String playMysic() {
         return "Shopen";
+    }
+
+    public void doMyInit() {
+        System.out.println("init");
+    }
+    public void doMyDestroy(){
+        System.out.println("destroy");
     }
 }
--- src/main/java/SpringTest.java
+++ src/main/java/SpringTest.java
@@ -1,5 +1,8 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpringTest {
     // https://github.com/NeilAlishev/SpringCourse
     public static void main(String[] args) {
@@ -8,15 +11,25 @@ public class SpringTest {
 //        TestBean testBean = context.getBean("TestBean", TestBean.class);
 //        System.out.println(testBean.getName());
 
-        MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer mp2 = context.getBean("musicPlayer", MusicPlayer.class);
-        System.out.println(mp.equals(mp2));
-        mp.playMusic();
-
-        mp.setVolume(33);
-        System.out.println(mp2.getVolume());
+//        MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer mp2 = context.getBean("musicPlayer", MusicPlayer.class);
+//        System.out.println(mp.equals(mp2));
+//        mp.playMusic();
+//
+//        mp.setVolume(33);
+//        System.out.println(mp2.getVolume());
+//        EmptyClass emptyClass = new EmptyClass();
 //        System.out.println(mp.getName());
 //        System.out.println(mp.getVolume());
+
+        Music music = context.getBean("music", ClassicalMusic.class);
+        System.out.println(music.playMysic());
+
+//        EmptyClass emptyClass = new EmptyClass();
+//        List<Integer> list = emptyClass.getList();
+//        list.remove(0);
+//        list.add(1);
+//        System.out.println(emptyClass.getInt());
 
         context.close();
     }
