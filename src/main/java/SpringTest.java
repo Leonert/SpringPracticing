8c598e74818b53b0d0620b902c1a145c681e24a7
Leonert <leonid.knyshev@gmail.com>
2023-02-20 11:58:44 +0200

    learning differences between singleton and factory scopes

--- src/main/java/SpringTest.java
+++ src/main/java/SpringTest.java
@@ -9,7 +9,12 @@ public class SpringTest {
 //        System.out.println(testBean.getName());
 
         MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer mp2 = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(mp.equals(mp2));
         mp.playMusic();
+
+        mp.setVolume(33);
+        System.out.println(mp2.getVolume());
 //        System.out.println(mp.getName());
 //        System.out.println(mp.getVolume());
 
